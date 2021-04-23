c17b706df523fa976251e03ab6d7b65015cd80fa
André Melo <proandremelo@gmail.com>
2021-04-22 22:48:28 -0300

    Filled the calendar

--- js/calendar.js
+++ js/calendar.js
@@ -9,8 +9,40 @@ function init(){
     var actualMonth = months[today.getMonth()];
     var actualYear = today.getFullYear();
     document.getElementById("label-date").innerHTML = actualMonth + ", " + actualYear;
+    var daysInCalendar =  document.getElementById("month").querySelectorAll("input");
+    var thisMonth = new Date(today.getFullYear(),today.getMonth());
+    for(let i = 0, day = thisMonth.getDate(), week = thisMonth.getDay();
+     i < getAllDaysInAMonth(thisMonth.getMonth(),thisMonth.getFullYear()); i++, day++, week++){
+        daysInCalendar[week].value = day;
+    }
 }
 
 function getAllDaysInAMonth(month,year){
-    var respectiveDate = new Date();
+    let maxDays = {
+        "0" : 31,
+        "1" : 28,
+        "2" : 31,
+        "3" : 30,
+        "4" : 31,
+        "5" : 30,
+        "6" : 31,
+        "7" : 31,
+        "8" : 30,
+        "9" : 31,
+        "10" : 30,
+        "12" : 31
+    };
+    if(leapYear(year) && month == 1){
+        return maxDays[month]++;
+    }else{
+        return maxDays[month];
+    }
+}
+
+function leapYear(year){
+    if(year % 4 === 0){
+        return true;
+    }else{
+        return false;
+    }
 }
