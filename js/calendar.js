86defa8214d72bd0d567de4a150458ca0a0070dd
André Melo <proandremelo@gmail.com>
2021-04-27 12:12:56 -0300

    Calendar done. Need to add the events' functions.

--- js/calendar.js
+++ js/calendar.js
@@ -1,23 +1,31 @@
-window.addEventListener("DOMContentLoaded", init);
 
-function init(){
-    var months = ["January", "February", "March",
-                    "April", "May", "June", "July",
-                    "August", "September", "October",
-                    "November", "December"];
-    var today = new Date();
-    var actualMonth = months[today.getMonth()];
-    var actualYear = today.getFullYear();
-    document.getElementById("label-date").innerHTML = actualMonth + ", " + actualYear;
-    var daysInCalendar =  document.getElementById("month").querySelectorAll("div");
-    var thisMonth = new Date(today.getFullYear(),today.getMonth());
-    for(let i = 0, day = thisMonth.getDate(), week = thisMonth.getDay();
-     i < getAllDaysInAMonth(thisMonth.getMonth(),thisMonth.getFullYear()); i++, day++, week++){
-        daysInCalendar[week].innerHTML = day;
-    }
-}
 
-function getAllDaysInAMonth(month,year){
+function Calendar(){
+    this.date = new Date();
+    this.months = ["January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"];
+    this.days = document.getElementById("month").querySelectorAll("div");
+};
+Calendar.prototype.applyDate = function(dateToBeApplied){
+    console.log("Calling applyDate");
+    document.getElementById("label-date").innerHTML = 
+        this.months[dateToBeApplied.getMonth()] + ", " + dateToBeApplied.getFullYear();
+    dateToBeApplied.setDate(1);
+    this.days.forEach(day => day.innerHTML = "");
+    for(let i = 0; i < getAllDaysInAMonth(dateToBeApplied.getFullYear(),dateToBeApplied.getMonth()); i++){
+        this.days[i + dateToBeApplied.getDay()].innerHTML = i + 1;
+    };
+};
+Calendar.prototype.events = function(){
+    
+};
+Calendar.prototype.addEvent = function(){
+
+};
+
+function getAllDaysInAMonth(year,month){
     let maxDays = {
         "0" : 31,
         "1" : 28,
@@ -30,7 +38,7 @@ function getAllDaysInAMonth(month,year){
         "8" : 30,
         "9" : 31,
         "10" : 30,
-        "12" : 31
+        "11" : 31
     };
     if(leapYear(year) && month == 1){
         return maxDays[month]++;
@@ -46,3 +54,38 @@ function leapYear(year){
         return false;
     }
 }
+
+var calendar = new Calendar();
+
+window.addEventListener("DOMContentLoaded", calendar.applyDate(calendar.date));
+document.getElementById("backward").addEventListener("click", function(){
+    if(calendar.date.getMonth()-1 == -1){
+        calendar.date = new Date(calendar.date.getFullYear()-1, 11);
+    }else{
+        calendar.date = new Date(calendar.date.getFullYear(), calendar.date.getMonth()-1);
+    }
+    calendar.applyDate(calendar.date);
+});
+document.getElementById("forward").addEventListener("click", function(){
+    if(calendar.date.getMonth()+1 == 12){
+        calendar.date = new Date(calendar.date.getFullYear()+1, 0);
+    }else{
+        calendar.date = new Date(calendar.date.getFullYear(), calendar.date.getMonth()+1);
+    }
+    calendar.applyDate(calendar.date);
+});
+document.getElementById("today").addEventListener("click", function(){
+    calendar.date = new Date();
+    calendar.applyDate(calendar.date);
+});
+document.getElementById("events").addEventListener("click", function(){
+    window.alert("event clicked!");
+});
+
+var months = document.getElementById("month").querySelectorAll("div");
+console.log(months);
+months.forEach(month => month.addEventListener("click", function(evt){
+    if(evt.target.innerHTML != 0){
+        window.alert("Day clicked: " + evt.target.innerHTML);
+    };
+}));
