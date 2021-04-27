3be676dea6b11c967defaaca41953645bf127219
André Melo <proandremelo@gmail.com>
2021-04-27 18:15:07 -0300

    Solved problem with the position of the actual day

--- js/calendar.js
+++ js/calendar.js
@@ -7,12 +7,25 @@ function Calendar(){
     "August", "September", "October",
     "November", "December"];
     this.days = document.getElementById("month").querySelectorAll("div");
+    // this.actualDay = this.days[this.date.getDay() + this.date.getDate() - 1];
+    this.actualDay = this.date.getDate();
+    this.actualDayPosition = 0;
+    this.actualWeekDay = this.date.getDay();
+    this.actualMonth = this.date.getMonth();
+    this.actualYear = this.date.getFullYear();
 };
-Calendar.prototype.applyDate = function(dateToBeApplied){
-    // console.log("Calling applyDate");
+Calendar.prototype.applyDate = function(evt, dateToBeApplied){
     document.getElementById("label-date").innerHTML = 
         this.months[dateToBeApplied.getMonth()] + ", " + dateToBeApplied.getFullYear();
     dateToBeApplied.setDate(1);
+    if(evt.type == "DOMContentLoaded" || evt.target.id == "today" || 
+    (dateToBeApplied.getFullYear() == this.actualYear && dateToBeApplied.getMonth() == this.actualMonth)){
+        this.actualDayPosition = this.actualDay + dateToBeApplied.getDay() - 1;
+        this.days[this.actualDayPosition].style.borderColor = "red";
+        
+    }else{
+        this.days[this.actualDayPosition].style.borderColor = "";
+    }
     this.days.forEach(day => day.innerHTML = "");
     let allDaysInAMonth = getAllDaysInAMonth(dateToBeApplied.getFullYear(),dateToBeApplied.getMonth());
     for(let i = 0; i < allDaysInAMonth; i++){
@@ -60,29 +73,31 @@ function leapYear(year){
 
 var calendar = new Calendar();
 
-window.addEventListener("DOMContentLoaded", calendar.applyDate(calendar.date));
-document.getElementById("backward").addEventListener("click", function(){
+window.addEventListener("DOMContentLoaded", (evt) => {
+    calendar.applyDate(evt, calendar.date);
+});
+document.getElementById("backward").addEventListener("click", (evt) => {
     if(calendar.date.getMonth()-1 == -1){
         calendar.date = new Date(calendar.date.getFullYear()-1, 11);
     }else{
         calendar.date = new Date(calendar.date.getFullYear(), calendar.date.getMonth()-1);
     }
-    calendar.applyDate(calendar.date);
+    calendar.applyDate(evt, calendar.date);
 });
-document.getElementById("forward").addEventListener("click", function(){
+document.getElementById("forward").addEventListener("click", (evt) => {
     if(calendar.date.getMonth()+1 == 12){
         calendar.date = new Date(calendar.date.getFullYear()+1, 0);
     }else{
         calendar.date = new Date(calendar.date.getFullYear(), calendar.date.getMonth()+1);
     }
-    calendar.applyDate(calendar.date);
+    calendar.applyDate(evt, calendar.date);
 });
-document.getElementById("today").addEventListener("click", function(){
+document.getElementById("today").addEventListener("click", (evt) => {
     calendar.date = new Date();
-    calendar.applyDate(calendar.date);
+    calendar.applyDate(evt, calendar.date);
 });
-document.getElementById("events").addEventListener("click", function(){
-    window.alert("event clicked!");
+document.getElementById("events").addEventListener("click", (evt) => {
+    window.openDialog("../html/test.html");
 });
 
 var months = document.getElementById("month").querySelectorAll("div");
